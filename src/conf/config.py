f835d6e43b4da75864df833b11a996608654f07d
Mateo <macalvo@fi.uba.ar>
2022-10-21 02:41:50 -0300

    Fix settings

--- src/conf/config.py
+++ src/conf/config.py
@@ -5,5 +5,5 @@ import os
 class Settings(BaseSettings):
     DATABASE_URI: str = os.environ.get("DATABASE_URI")
     API_V1_STR: str = os.environ.get("API_VERSION_PREFIX")
-    APP_ENV: os.environ.get("APP_ENV")
+    APP_ENV: str = os.environ.get("APP_ENV")
     TEST_ENV: str = 'testing'
