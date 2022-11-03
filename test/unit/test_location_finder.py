f636149d79d9d9391cdbf2bbf70ea7b044feab57
mateoicalvo <macalvo@fi.uba.ar>
2022-11-03 04:08:03 -0300

    Add more tests

--- test/unit/test_location_finder.py
+++ test/unit/test_location_finder.py
@@ -5,3 +5,9 @@ from src.conf.config import Settings
 def test_location_finder_creation():
     finder = LocationFinder(Settings().APP_ENV)
     assert finder is not None
+
+
+def test_location_finder():
+    finder = LocationFinder(Settings().APP_ENV)
+    found = finder.find_by_address('Av. Paseo Colón 850, Buenos Aires')
+    assert found.address == 'Av. Paseo Colón 850, Buenos Aires'
